b480ea29e6d7a06649ac335c7f3c33c019eec448
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2022-05-03 08:46:33 -1200

    WAtchtower

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  function about() {
+    window.location.href = "About.html"
+  }
+
+  function validate() {
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('tt').value;
+    const phoneNo = document.getElementById('dd').value;
+    
+    if (name === "") {
+      alert("Pls fill in your name");
+      return false;
+    } else if (email ===""){
+      alert("Pls fill in your email");
+      return false;
+    } else if (phoneNo ==="") {
+      alert("Pls fill in your phone No")
+      return false;
+    } else {
+      alert ("Welcome to the fold of christ, Youll get a message from us soon")
+      return true;
+    }
+  }
