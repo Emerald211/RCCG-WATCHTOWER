91c323b877fd954ae9c25a9af0e6381b7966e6ed
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2022-05-05 05:22:36 -1200

    watchtower

--- script.js
+++ script.js
@@ -31,4 +31,31 @@ function openNav() {
     }
   }
 
- 
+
+  function myFunction() {
+    document.getElementById('myDropdown').classList.toggle('show')
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+function openPopup() {
+  let popup = document.getElementById("popup");
+  popup.classList.add("open-popup")
+}
+
+function closePopup() {
+  let popup = document.getElementById("popup");
+  popup.classList.remove("open-popup")
+}
